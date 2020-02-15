005654e8ef1cc63f04792d5576311a28a4f65149
Nana Aferi <nana.aferi@meltwater.com>
2020-02-15 17:03:43 +0000

    init comit
    Signed-off-by: Nana Aferi <nana.aferi@meltwater.com>

---
+++ DirectionsApp/DirectionsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DirectionsApp
+//
+//  Created by Nana Aferi on 2/15/20.
+//  Copyright © 2020 ANK Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
