669bd269e8a41b14c8d7412ee0508a770ca9100b
Nana Aferi <nana.aferi@meltwater.com>
2020-02-16 00:38:21 +0000

    Display current position of a user on a map
    Signed-off-by: Nana Aferi <nana.aferi@meltwater.com>

--- DirectionsApp/DirectionsApp/ViewController.swift
+++ DirectionsApp/DirectionsApp/ViewController.swift
@@ -1,20 +1,171 @@
 //
 //  ViewController.swift
-//  DirectionsApp
+//  GetDirectionsDemo
 //
-//  Created by Nana Aferi on 2/15/20.
-//  Copyright © 2020 ANK Development. All rights reserved.
+//  Created by Alex Nagy on 12/02/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import Layoutless
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var steps: [MKRoute.Step] = []
+    var stepCounter = 0
+    var route:MKRoute?
+    var showMapRoute = false
+    var navigationStarted = false
+    var locationDistance:Double = 500
+    
+    var speechSynthesizer  = AVSpeechSynthesizer()
+    
+    lazy var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            handleAuthorizationStatus(locationManager: locationManager, status: CLLocationManager.authorizationStatus())
+        }else{
+            debugPrint("Location services are not enabled")
+        }
+        
+        return locationManager
+        
+    }()
+    
+    lazy var directionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Where do you want to go?"
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var textField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter your destination"
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
+    
+    lazy var getDirectionButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Get Direction", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(getDirectionButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var startStopButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Start Navigation", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(startStopButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        return mapView
+    }()
+    
+    @objc fileprivate func getDirectionButtonTapped() {
+        
+    }
+    
+    @objc fileprivate func startStopButtonTapped() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
+        locationManager.startUpdatingHeading()
     }
 
+    fileprivate func setupViews() {
+        view.backgroundColor = .white
+        
+        stack(.vertical)(
+            directionLabel.insetting(by: 16),
+            stack(.horizontal, spacing: 16)(
+                textField,
+                getDirectionButton
+            ).insetting(by: 16),
+            startStopButton.insetting(by: 16),
+            mapView
+        ).fillingParent(relativeToSafeArea: true).layout(in: view)
+    }
+    
+    fileprivate func centerViewToUserLocation(center: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: locationDistance, longitudinalMeters: locationDistance)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    fileprivate func handleAuthorizationStatus(locationManager: CLLocationManager, status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //
+            break
+        case .denied:
+            //
+            break
+        case .authorizedAlways:
+            //
+            break
+        case .authorizedWhenInUse:
+            if let center = locationManager.location?.coordinate {
+                centerViewToUserLocation(center: center)
+            }
+            break
+        @unknown default:
+            
+            break
+            
+        }
+        
+    }
+    
+    fileprivate func mapRoute(destinationCoordinate: CLLocationCoordinate2D) {
+        
+    }
+    
+    fileprivate func getRouteSteps(route: MKRoute) {
+        
+    }
 
 }
 
+extension ViewController : CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !showMapRoute {
+            if let location = locations.last {
+                let center = location.coordinate
+                centerViewToUserLocation(center: center)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        handleAuthorizationStatus(locationManager: locationManager, status: status)
+    }
+}
+
+extension ViewController : MKMapViewDelegate {
+    
+}
---
+++ DirectionsApp/Pods/Target Support Files/Pods-DirectionsApp/Pods-DirectionsApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DirectionsApp : NSObject
+@end
+@implementation PodsDummy_Pods_DirectionsApp
+@end
